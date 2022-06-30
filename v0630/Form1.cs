a97d8f5b0e18648759dc73c2398f883af053e642
A601-11 <tnk.dat.class@gmail.com>
2022-06-30 13:54:34 +0900

    v0630(6)

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -25,6 +25,7 @@ namespace v0630
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Enabled = true;
+            timer1.Interval = 30;
             label1.Left += vx;
             label1.Top += vy;
 
